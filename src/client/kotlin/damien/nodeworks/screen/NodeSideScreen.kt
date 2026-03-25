7f974eb3a7cc0ae81b28ce9348c97ad95266bddc
Damien Stamates <thedoubledimensions@gmail.com>
2026-03-25 19:20:02 -0400

    better inventory

--- src/client/kotlin/damien/nodeworks/screen/NodeSideScreen.kt
+++ src/client/kotlin/damien/nodeworks/screen/NodeSideScreen.kt
@@ -3,6 +3,7 @@ package damien.nodeworks.screen
 import damien.nodeworks.screen.NodeSideScreenHandler
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.client.renderer.RenderPipelines
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.Identifier
 import net.minecraft.world.entity.player.Inventory
@@ -19,23 +20,19 @@ class NodeSideScreen(
 
     init {
         imageWidth = 176
-        imageHeight = 172
+        imageHeight = 166
         inventoryLabelY = imageHeight - 94
     }
 
     override fun renderBg(graphics: GuiGraphics, partialTick: Float, mouseX: Int, mouseY: Int) {
-        // Draw a simple dark background
-        graphics.fill(leftPos, topPos, leftPos + imageWidth, topPos + imageHeight, 0xFFC6C6C6.toInt())
-        // Darker border
-        graphics.fill(leftPos, topPos, leftPos + imageWidth, topPos + 1, 0xFF555555.toInt())
-        graphics.fill(leftPos, topPos + imageHeight - 1, leftPos + imageWidth, topPos + imageHeight, 0xFF555555.toInt())
-        graphics.fill(leftPos, topPos, leftPos + 1, topPos + imageHeight, 0xFF555555.toInt())
-        graphics.fill(leftPos + imageWidth - 1, topPos, leftPos + imageWidth, topPos + imageHeight, 0xFF555555.toInt())
-
-        // Card slot highlight (slot 0 is at x=80, y=18 relative to the menu)
-        val cardX = leftPos + 80 - 1
-        val cardY = topPos + 18 - 1
-        graphics.fill(cardX, cardY, cardX + 18, cardY + 18, 0xFF8BE08E.toInt())
+        graphics.blit(
+            RenderPipelines.GUI_TEXTURED,
+            BACKGROUND,
+            leftPos, topPos,
+            0f, 0f,
+            imageWidth, imageHeight,
+            256, 256
+        )
     }
 
     override fun render(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
