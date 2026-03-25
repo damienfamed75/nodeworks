33019b1cd6052ab3582e8a91dbd30d08b2a99593
Damien Stamates <thedoubledimensions@gmail.com>
2026-03-25 11:43:37 -0400

    init

---
+++ src/client/kotlin/damien/nodeworks/NodeworksClient.kt
@@ -0,0 +1,9 @@
+package damien.nodeworks
+
+import net.fabricmc.api.ClientModInitializer
+
+object NodeworksClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
